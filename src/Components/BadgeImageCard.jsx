fc28d8475e55e0e2ec0db232e418604f48627318
ShubhamRaut187 <raut.shubham4554@gmail.com>
2024-02-17 17:14:26 +0530

    Feature Added,Deploy

--- src/Components/BadgeImageCard.jsx
+++ src/Components/BadgeImageCard.jsx
@@ -3,50 +3,54 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import {styled} from '@mui/material/styles'
+
 function BadgeImageCard({Image,open,setOpen}) {
-   console.log(open);
+//    console.log(open);
     const handleOpen = () => setOpen(true);
    const handleClose = () => {
-    window.location.reload()
-    // setOpen(false)
-    // console.log(open);
-   };
+    setOpen(false)
+};
     const style = {
         position: 'absolute',
         top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%)',
-        width: 300, // Adjust the width here
+        width: 300, 
+        height:150,
         bgcolor: 'background.paper',
-        boxShadow: 24,
+        boxShadow: 'none',
+        borderRadius: '8px',
         p: 4,
+        
       };
-      const smallImgStyle = {
-        width: 50,
-        height: 50,
-        borderRadius: '50%',
-        marginRight: 2,
-    }
     return (
+        <>
         <div className='badge_image_card' onClick={handleOpen}>
+        
+            <img src={Image} alt="Badge" />
+        </div>
         <Modal
         open={open}
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
+        BackdropProps={{invisible: true}}
         >
         <Box sx={style}>
             <Button onClick={handleClose} style={{ 'position': 'absolute', 'top': 0, 'right': 0 }}>
                 Close
             </Button>
-            <img src={Image} alt="Small Img" style={smallImgStyle} />
-                <Typography variant="body1" component="p">
-                    Text in a modal. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            <img src={Image} alt="Small Img" style={{ width: '20%', maxWidth: 200, display: 'block', margin: '0 auto', borderRadius: 8 }} />
+                <Typography variant="body1" component="p" style={{textAlign:'center',fontSize:'20px',fontWeight:'700'}}>
+                    Badge Unclocked!⭐
+                 </Typography>
+                <Typography variant="body1" component="p" style={{textAlign:'center',color:'gray'}}>
+                🎉Level up! Earned a shiny new badge!🏅⭐
                 </Typography>
         </Box>
         </Modal>
-            <img src={Image} alt="Badge" />
-        </div>
+        </>
     );
 }
 
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -7,7 +7,7 @@ function Dashboard(props) {
     let [User,SetUser] = useState({});
     let [Points,SetPoints] = useState({});
     let [Badges,SetBadges] = useState([]);
-    
+    let [Rank,SetRank] = useState({});
     useEffect(()=>{
         let getdata = async() => {
             try {
@@ -22,6 +22,42 @@ function Dashboard(props) {
                 let response_user = await reults_user.json();
                 // console.log(response_user);
                 SetUser(response_user.data);
+
+                let results_points = await fetch(`https://staging.questprotocol.xyz/api/entities/e-0000000000/users/u-a2399489-9cd0-4c94-ad12-568379202b08/xp`,{
+                    headers:{
+                        'accept' : 'application/json',
+                        'apikey': 'k-6fe7e7dc-ac8f-44a1-8bbf-a1754ddf88be',
+                        'token' : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ1LWEyMzk5NDg5LTljZDAtNGM5NC1hZDEyLTU2ODM3OTIwMmIwOCIsImlhdCI6MTcwNzk4NzYyOSwiZXhwIjoxNzA4NTkyNDI5fQ.fESDqKunAqLUgHBCUsNYpGcNrTeVEty91HqGebX59Uc',
+                        'userid' : 'u-a2399489-9cd0-4c94-ad12-568379202b08'
+                    }
+                });
+                let response_points = await results_points.json();
+                // console.log(response_points); 
+                SetPoints(response_points);
+
+                let results_rank = await fetch(`https://staging.questprotocol.xyz/api/entities/e-0000000000/users/u-a2399489-9cd0-4c94-ad12-568379202b08/xp-leaderboard-rank`,{
+                    headers:{
+                        'accept' : 'application/json',
+                        'apikey': 'k-6fe7e7dc-ac8f-44a1-8bbf-a1754ddf88be',
+                        'token' : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ1LWEyMzk5NDg5LTljZDAtNGM5NC1hZDEyLTU2ODM3OTIwMmIwOCIsImlhdCI6MTcwNzk4NzYyOSwiZXhwIjoxNzA4NTkyNDI5fQ.fESDqKunAqLUgHBCUsNYpGcNrTeVEty91HqGebX59Uc',
+                        'userid' : 'u-a2399489-9cd0-4c94-ad12-568379202b08'
+                    }
+                });
+                let response_rank = await results_rank.json();
+                // console.log(response_rank);
+                SetRank(response_rank.data);
+
+                let results_badges = await fetch(`https://staging.questprotocol.xyz/api/entities/e-0000000000/users/u-a2399489-9cd0-4c94-ad12-568379202b08/badges?limit=9`,{
+                    headers:{
+                        'accept' : 'application/json',
+                        'apikey': 'k-6fe7e7dc-ac8f-44a1-8bbf-a1754ddf88be',
+                        'token' : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ1LWEyMzk5NDg5LTljZDAtNGM5NC1hZDEyLTU2ODM3OTIwMmIwOCIsImlhdCI6MTcwNzk4NzYyOSwiZXhwIjoxNzA4NTkyNDI5fQ.fESDqKunAqLUgHBCUsNYpGcNrTeVEty91HqGebX59Uc',
+                        'userid' : 'u-a2399489-9cd0-4c94-ad12-568379202b08'
+                    }
+                });
+                let response_badges = await results_badges.json();
+                // console.log(response_badges);
+                SetBadges(response_badges.data);
             } catch (error) {
                 console.log(error);
             }
@@ -44,15 +80,15 @@ function Dashboard(props) {
                 </div>
                 <div className='badges_div'>
                     <div className='badge_card'>
-                        <p className='badge_card_info'>2100</p>
+                        <p className='badge_card_info'>{Points.data}</p>
                         <p className='badge_card_title'>Points</p>
                     </div>
                     <div className='badge_card'>
-                        <p className='badge_card_info'>#1</p>
+                        <p className='badge_card_info'>#{Rank.position}</p>
                         <p className='badge_card_title'>Rank</p>
                     </div>
                     <div className='badge_card'>
-                        <p className='badge_card_info'>3</p>
+                        <p className='badge_card_info'>{Points.tier}</p>
                         <p className='badge_card_title'>Level</p>
                     </div>
                     
@@ -63,9 +99,20 @@ function Dashboard(props) {
                     <button className='ord_button'>Point History</button>
                 </div>
                 <div className='badge_option_div'>
-                    <BadgeImageCard Image={'https://images.unsplash.com/photo-1621155346337-1d19476ba7d6?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGltYWdlfGVufDB8fDB8fHww'} open={open} setOpen={setOpen}/>
+                    {
+                        Badges ? Badges.map((elem,index)=>{
+                            return <BadgeImageCard key={index} Image={elem.imageUrl} open={open} setOpen={setOpen}/>
+                        }) : <></>
+                    }
                     
                 </div>
+                
+            </div>
+            <div className='footer'>
+            <i className="fa-solid fa-star fa-lg" style={{'color': '#313131'}}></i>
+            <i className="fa-solid fa-magnifying-glass fa-lg" style={{'color': '#313131'}}></i>
+            <i className="fa-solid fa-bag-shopping fa-lg" style={{'color': '#313131'}}></i>
+            <i className="fa-solid fa-user fa-lg" style={{'color': '#313131'}}></i>
             </div>
         </div>
     );
